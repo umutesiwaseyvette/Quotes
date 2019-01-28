3985e91288ae2bd2f7996052615fe0204c0fdcd2
umutesiwase yvette <rwjpyy@gmail.com>
2019-01-28 15:02:47 +0200

    add folder

--- app.module.ts
+++ app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule} from "@angular/forms"
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuotesComponent } from './quotes/quotes.component';
 import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
+import { QuoteComponent } from './quote/quote.component';
+import { HighlightDirective } from './highlight.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormComponent,
+    DetailsComponent,
     QuoteComponent,
-    QuotesComponent,
-    FormComponent
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- quote.ts
+++ quote.ts
@@ -1,3 +1,11 @@
+
 export class Quote {
-    constructor(public name:string, public quote:string, public author:string){}
-}
+    public upvotes:number
+    public downvotes:number
+    public myDate:Date
+    constructor(public name:string, public author:string, public quote:string){
+      this.upvotes=0
+      this.downvotes=0
+      this.myDate= new Date()
+      }
+  }
